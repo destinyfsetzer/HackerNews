c829776434c7b5ef5ef5cf39d2166a60efe1b904
destinyfsetzer <54375774+destinyfsetzer@users.noreply.github.com>
2020-09-24 16:28:39 -0500

    It display it okay

--- src/components/DisplayArticleCard.js
+++ src/components/DisplayArticleCard.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react'
 
 class DisplayArticleCard extends Component {
   render() {
-    const {url, title, author, created_at, points} = this.props.article;
+    const items = this.props.items
+    // const {url, title, author, created_at, points} = this.props.article;
     return (
       <div>
-        <h6> {url} </h6>
-        <a href = {url}>
-          <h3> {title} </h3>
+        <h6> {items.url} </h6>
+        <a href = {items.url}>
+          <h3> {items.title} </h3>
         </a>
-        <h4> {author} | {created_at} | {points} </h4>
+        <h4> {items.author} | {items.created_at} | {items.points} </h4>
       </div>
     )
   }
